13df6bf3838f8b730fcb8796092e09c442814ea4
siljgonzalez-web <siljgonzalez@gmail.com>
2026-08-14 14:34:49 -0300

    Actualizar cache Mi Control v22.4

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const C='mi-control-v20';const A=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='mi-control-v22-4';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
