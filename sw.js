fe5494fb07e77bcdd17e5dc74c2476446087662d
siljgonzalez-web <siljgonzalez@gmail.com>
2026-08-14 16:08:35 -0300

    Renovar cache PWA v22.9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mi-control-v22-4';
-self.addEventListener('install',e=>{self.skipWaiting();});
+const C='mi-control-v22-9';
+self.addEventListener('install',()=>{self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
